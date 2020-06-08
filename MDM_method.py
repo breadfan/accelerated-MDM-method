740184056314aa800ff7b6f367ec47d9d1b7e70b
breadfan <kizeevdanil@yandex.ru>
2020-06-08 04:50:22 +0300

    Removed duplicate rows;after finding cycle program now isnot repeating, but continuing from place in the past(needed new variables to save all of this data)

--- MDM_method.py
+++ MDM_method.py
@@ -45,19 +45,24 @@ class MDM(object):
 
 
     def solve(self):
+        V = 0
+        iterations = 0
+
         delta_p = 1
         p_vector = [0 for i in range(0, len(self._points))]
         supp_vector = []
         t_param_vector =[]
+
         MIN_set = []
         MAX_set = []
-
         diff_vector = []                            #for cycles finding
+        P_vectors = []                              #matrix of p_vectors
+        V_vectors = []
         cycle_constructed = False
         cycle_is_constructing = False
+        special_upd_done = False                    #whether special update Wn = W + lambdaV is done
         cycle_current_size = 0                      #we will search actual size of cycle
 
-        iterations = 0
         initial_approximation = 1                    #it can be changed for lowering iterations sake;
         #for first approximation we'll just take point from a board of hull - cause it's easy reduced
         vector_current = self._points[self._hull.vertices[initial_approximation]].copy()    #need copy() there for non-changing _points
@@ -66,168 +71,95 @@ class MDM(object):
         #then we need to find vect_{k+1} iteratively
 
         while delta_p > 0.000001 and iterations < 500 and len(supp_vector) != 0:
+            if cycle_constructed is True and special_upd_done is False:
+                for i in range(cycle_size):  #constructing V as linear combination of D's that we used previously
+                    V += -1 * t_param_vector[cycle_start + i] * diff_vector[cycle_start + i]
+                p_vector = P_vectors[cycle_start]                   #returning to value where cycle had begun
+                vector_current = V_vectors[cycle_start]       #returning
+                supp_vector = []                        #returning
+                for i in range(len(p_vector)):          #returning
+                    if p_vector[i] > 0.0000001:
+                        supp_vector.append(i)
+
+                lambda_t = -np.dot(vector_current, V) / np.linalg.norm(V) ** 2
+                for i in range(cycle_size):
+                    if t_param_vector[i] > 0:
+                        if lambda_t > (1 - p_vector[MIN_set[i]]) / t_param_vector[i]:
+                            lambda_t = (1 - p_vector[MIN_set[i]]) / t_param_vector[i]
+                    elif t_param_vector[i] < 0:
+                        if lambda_t > -p_vector[MAX_set[i]] / t_param_vector[i]:
+                            lambda_t = -p_vector[MAX_set[i]] / t_param_vector[i]
+                vector_current += lambda_t * V
+                for i in range(cycle_size):
+                    p_vector[MAX_set[i]] -= lambda_t * t_param_vector[i]
+                    p_vector[MIN_set[i]] += lambda_t * t_param_vector[i]
+                special_upd_done = True     #once it's done we're forgiving about that
+
+
+            mult = np.dot(self._points[supp_vector], vector_current)
+            ind_max = np.argmax(mult)           #finding max for indices in supp_vector
+            ind_max = supp_vector[ind_max]      #finding max general in our mult product
+
+            mult = np.matmul(vector_current, self._A_matrix)
+            ind_min = np.argmin(mult)                                                 # i''_k
             if cycle_constructed is False:
-                mult = np.dot(self._points[supp_vector], vector_current)
-                ind_max = np.argmax(mult)           #finding max for indices in supp_vector
-                ind_max = supp_vector[ind_max]      #finding max general in our mult product
-                MAX_set.append(ind_max)
-
-                mult = np.matmul(vector_current, self._A_matrix)
-                ind_min = np.argmin(mult)                                                 # i''_k
                 MIN_set.append(ind_min)
+                MAX_set.append(ind_max)
+            diff = self._points[ind_max] - self._points[ind_min]
+            print('\nDifference: ' + str(diff))
+            delta_p = np.dot(diff, vector_current)
 
-                diff = self._points[ind_max] - self._points[ind_min]
-                print('\nDifference: ' + str(diff))
-                delta_p = np.dot(diff, vector_current)
+            if delta_p > 0.000001:                  #if not bigger, then we've found a solution
+                print('delta_p: ' + str(delta_p))
+                print('p_vector[ind_max] = ' + str(p_vector[ind_max]) + '\nnp.linalg.norm(diff)): '
+                      + str(np.linalg.norm(diff)))
+                t_param = delta_p /(p_vector[ind_max] * (np.linalg.norm(diff)) ** 2)  # recounting all variables
+                if t_param >= 1:
+                    t_param = 1
 
-                if delta_p > 0.000001:                  #if not bigger, then we've found a solution
-                    print('delta_p: ' + str(delta_p))
-                    print('p_vector[ind_max] = ' + str(p_vector[ind_max]) + '\nnp.linalg.norm(diff)): '
-                          + str(np.linalg.norm(diff)))
-                    t_param = delta_p /(p_vector[ind_max] * (np.linalg.norm(diff)) ** 2)  # recounting all variables
-                    if t_param >= 1:
-                        t_param = 1
-                    if iterations > 0 and cycle_is_constructing is False:  #constructing cycle(active finding cycle, i mean, active-active)
-                        contains = np.where(np.all(diff_vector == diff, axis = 1))[0]    #finds if diff_vector contains diff
-                        if len(contains) != 0:      #found first element of cycle
-                            cycle_is_constructing = True        #cycle is constructing now
-                            cycle_start = contains[0]                     #index of first element of cycle; not changing
-                            cycle_size = iterations - cycle_start         #not changing
-                            cycle_current_size += 1         #this var for checking if all variables actually are cycle
-                            p_vectorCS = p_vector.copy()
-                            vector_currentCS = vector_current.copy()
-                        t_param_vector.append(t_param)      #saving t_params for constructing V in the future
-                        diff_vector.append(diff)            #saving D_i
-                    elif cycle_is_constructing is True and cycle_constructed is False:
-                        if cycle_current_size < cycle_size and \
-                                np.where(np.all(diff_vector == diff, axis = 1))[0] \
-                                    == (cycle_start + cycle_current_size):
-                                    cycle_current_size += 1
-                                    t_param_vector.append(t_param)
-                                    diff_vector.append(diff)
-                                    t_param_vector.append(t_param)
-                        else:
-                            cycle_constructed = True
-                            print('CYCLE FOUND AND CONSTRUCTED SUCCESSFULLY!')
-                            delta_p = 0
-                    elif iterations == 0:
-                        t_param_vector.append(t_param)
+                if iterations > 0 and cycle_is_constructing is False:  #constructing cycle(active finding cycle, i mean, active-active)
+                    contains = np.where(np.all(diff_vector == diff, axis = 1))[0]    #finds if diff_vector contains diff
+                    if len(contains) != 0:      #found first element of cycle
+                        cycle_is_constructing = True        #cycle is constructing now
+                        cycle_start = contains[0]                     #index of first element of cycle; not changing
+                        cycle_size = iterations - cycle_start         #not changing
+                        cycle_current_size += 1         #this var for checking if all variables actually are cycle
+                    P_vectors.append(p_vector.copy())
+                    V_vectors.append(vector_current.copy())
+                    t_param_vector.append(t_param)      #saving t_params for constructing V in the future
+                    diff_vector.append(diff)            #saving D_i
+                elif cycle_is_constructing is True and cycle_constructed is False:
+                    if cycle_current_size < cycle_size and \
+                            np.where(np.all(diff_vector == diff, axis = 1))[0] \
+                            == (cycle_start + cycle_current_size):
+                        cycle_current_size += 1
                         diff_vector.append(diff)
+                        t_param_vector.append(t_param)
+                    else:
+                        cycle_constructed = True
+                        print('CYCLE FOUND AND CONSTRUCTED SUCCESSFULLY!')
+                elif iterations == 0:
+                    P_vectors.append(p_vector.copy())
+                    V_vectors.append(vector_current.copy())
+                    t_param_vector.append(t_param)
+                    diff_vector.append(diff)
 
 
-                    vector_current -= t_param * p_vector[ind_max] * diff
-                    supp_vector = []                #recounting
-                    temp1 = t_param * p_vector[ind_max]
-                    temp2 = (1 - t_param)
-                    p_vector[ind_min] += temp1
-                    p_vector[ind_max] *= temp2
+                vector_current -= t_param * p_vector[ind_max] * diff
+                supp_vector = []                #recounting
+                temp1 = t_param * p_vector[ind_max]
+                temp2 = (1 - t_param)
+                p_vector[ind_min] += temp1
+                p_vector[ind_max] *= temp2
 
-                    for i in range(len(p_vector)):
-                        if p_vector[i] > 0.0000001:
-                            supp_vector.append(i)
-                print('Vector current: ' + str(vector_current))
-                iterations += 1
-                print('Iterations: ' + str(iterations))
-                print('Supp_vector: ' + str(supp_vector))
-        if cycle_constructed is True:
-            V = 0           #constructing V as linear combination of D's that we used previously
-            for i in range(cycle_size):
-                   V += -1* t_param_vector[cycle_start + i] * diff_vector[cycle_start +i]
-            delta_p = 1
-            p_vector = [0 for i in range(0, len(self._points))]
-            supp_vector = []
-            initial_approximation = 1  # it can be changed for lowering iterations sake;
-                     #for first approximation we'll just take point from a board of hull - cause it's easy reduced
-            vector_current = self._points[
-                self._hull.vertices[initial_approximation]].copy()  # need copy() there for non-changing _points
-            supp_vector.append(self._hull.vertices[initial_approximation])  # approximation => get vect_0
-            p_vector[self._hull.vertices[initial_approximation]] = 1  # working right concat
-            for i in range(cycle_start):
-                mult = np.dot(self._points[supp_vector], vector_current)
-                ind_max = np.argmax(mult)  # finding max for indices in supp_vector
-                ind_max = supp_vector[ind_max]  # finding max general in our mult product
+                for i in range(len(p_vector)):
+                    if p_vector[i] > 0.0000001:
+                        supp_vector.append(i)
+            print('Vector current: ' + str(vector_current))
+            iterations += 1
+            print('Iterations: ' + str(iterations))
+            print('Supp_vector: ' + str(supp_vector))
 
-                mult = np.matmul(vector_current, self._A_matrix)
-                ind_min = np.argmin(mult)  # i''_k
-
-                diff = self._points[ind_max] - self._points[ind_min]
-                print('\nDifference: ' + str(diff))
-                delta_p = np.dot(diff, vector_current)
-                if delta_p > 0.0000001:  # if not bigger, then we've found a solution
-
-                    print('Delta: ' + str(delta_p))
-                    print('p_vector[ind_max] = ' + str(p_vector[ind_max]) + '\nnp.linalg.norm(diff): '
-                              + str(np.linalg.norm(diff)))
-                    t_param = delta_p / (
-                                 p_vector[ind_max] * (np.linalg.norm(diff)) ** 2)  # recounting all variables
-                    if t_param >= 1:
-                          t_param = 1
-
-                    vector_current -= t_param * p_vector[ind_max] * diff
-
-                    supp_vector = []  # recounting
-                    temp1 = t_param * p_vector[ind_max]
-                    temp2 = (1 - t_param)
-                    p_vector[ind_min] += temp1
-                    p_vector[ind_max] *= temp2
-                    for j in range(len(p_vector)):
-                        if p_vector[j] > 0.0000001:
-                               supp_vector.append(j)
-                    print('Vector current: ' + str(vector_current))
-                    iterations += 1
-                    print('Iterations: ' + str(iterations))
-                    print('Supp_vector: ' + str(supp_vector))
-
-            lambda_t = -np.dot(vector_current, V)/np.linalg.norm(V)**2
-            for i in range(cycle_size):
-                if t_param_vector[i] > 0:
-                     if lambda_t > (1 - p_vector[MIN_set[i]])/t_param_vector[i]:
-                          lambda_t = (1 - p_vector[MIN_set[i]])/t_param_vector[i]
-                elif t_param_vector[i] < 0:
-                     if lambda_t > -p_vector[MAX_set[i]]/t_param_vector[i]:
-                        lambda_t = -p_vector[MAX_set[i]]/t_param_vector[i]
-            vector_current += lambda_t*V
-            for i in range(cycle_size):
-                   p_vector[MAX_set[i]] -= lambda_t * t_param_vector[i]
-                   p_vector[MIN_set[i]] += lambda_t * t_param_vector[i]
-            ###
-            while delta_p > 0.000001  and iterations < 1000 and len(supp_vector) != 0:
-                if cycle_constructed is True:
-                    mult = np.dot(self._points[supp_vector], vector_current)
-                    ind_max = np.argmax(mult)  # finding max for indices in supp_vector
-                    ind_max = supp_vector[ind_max]  # finding max general in our mult product
-
-                    mult = np.matmul(vector_current, self._A_matrix)
-                    ind_min = np.argmin(mult)  #i''_k
-
-                    diff = self._points[ind_max] - self._points[ind_min]
-                    print('Diff: ' + str(diff))
-                    delta_p = np.dot(diff, vector_current)
-
-                    if delta_p > 0.000001:  #if not bigger, then we've found a solution
-
-                        print('\nDelta: ' + str(delta_p))
-                        print('p_vector[ind_max] = ' + str(p_vector[ind_max]) + '\nnp.linalg.norm(diff): '
-                              + str(np.linalg.norm(diff)))
-                        t_param = delta_p / (
-                                    p_vector[ind_max] * (np.linalg.norm(diff)) ** 2)  # recounting all variables
-                        if t_param >= 1:
-                            t_param = 1
-
-                        vector_current -= t_param * p_vector[ind_max] * diff
-
-                        supp_vector = []  #recounting
-                        temp1 = t_param * p_vector[ind_max]
-                        temp2 = (1 - t_param)
-                        p_vector[ind_min] += temp1
-                        p_vector[ind_max] *= temp2
-                        for i in range(len(p_vector)):
-                            if p_vector[i] > 0.0000001:
-                                supp_vector.append(i)
-                    print('Vector current: ' + str(vector_current))
-                    iterations += 1
-                    print('Iterations: ' + str(iterations))
-                    print('Supp_vector: ' + str(supp_vector))
         return vector_current
 
 
