1263a8a2ac62fbe31f85506614c8e282d963b2ed
breadfan <kizeevdanil@yandex.ru>
2020-06-11 15:46:50 +0300

    Reconstructing cycle-searching module. Founded that there are can exist > 1 cycles

--- MDM_method.py
+++ MDM_method.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 
 def GenerPoints(a, alpha, num_points, dim):
-    return alpha * np.random.rand(num_points, dim) + a
+    return alpha * np.random.randn(num_points, dim) + a
 
 def GetHullandPlot(points, dim):
     hull = ConvexHull(points)
@@ -117,33 +117,36 @@ class MDM(object):
                 if t_param >= 1:
                     t_param = 1
 
+                ###################
+                #Block for cycle finding. We must remember that MDM can improve wrong choices for updating vectors
+                #It could be labeled as "false cycle construction"
+                #####################
                 if self.isAccelerated is True:          #if using accelerated MDM-method
                     if iterations > 0 and cycle_is_constructing is False:  #constructing cycle(active finding cycle, i mean, active-active)
                         contains = np.where(np.all(diff_vector == diff, axis = 1))[0]    #finds if diff_vector contains diff
+                        print('np.where1: ' + str(contains))
                         if len(contains) != 0:      #found first element of cycle
                             cycle_is_constructing = True        #cycle is constructing now
-                            cycle_start = contains[0]                     #index of first element of cycle; not changing
+                            cycle_start = contains[len(contains)-1]                     #index of first element of cycle; not changing
                             cycle_size = iterations - cycle_start         #not changing
                             cycle_current_size += 1         #this var for checking if all variables actually are cycle
-                        P_vectors.append(p_vector.copy())
-                        V_vectors.append(vector_current.copy())
-                        t_param_vector.append(t_param)      #saving t_params for constructing V in the future
-                        diff_vector.append(diff)            #saving D_i
                     elif cycle_is_constructing is True and cycle_constructed is False:
-                        if cycle_current_size < cycle_size and \
-                                np.where(np.all(diff_vector == diff, axis = 1))[0] \
-                                == (cycle_start + cycle_current_size):
-                            cycle_current_size += 1
-                            diff_vector.append(diff)
-                            t_param_vector.append(t_param)
-                        else:
-                            cycle_constructed = True
-                            print('CYCLE FOUND AND CONSTRUCTED SUCCESSFULLY!')
-                    elif iterations == 0:
-                        P_vectors.append(p_vector.copy())
-                        V_vectors.append(vector_current.copy())
-                        t_param_vector.append(t_param)
-                        diff_vector.append(diff)
+                        if cycle_current_size < cycle_size:
+                            indices_match = np.where(np.all(diff_vector == diff, axis=1))[0]
+                            print('np.where2: ' + str(indices_match))
+                            if len(indices_match) > 0 and  indices_match[len(indices_match) - 1] == (cycle_start + cycle_current_size):
+                                cycle_current_size += 1     #checking if cycle exists
+                                if cycle_current_size == cycle_size:
+                                    cycle_constructed = True
+                                    print('CYCLE HAS FOUND AND CONSTRUCTED SUCCESSFULLY!')
+                            else:
+                                cycle_is_constructing = False
+                                cycle_current_size = 0
+                                print('False cycle construction.')
+                    P_vectors.append(p_vector.copy())
+                    V_vectors.append(vector_current.copy())
+                    t_param_vector.append(t_param)
+                    diff_vector.append(diff)
 
 
                 vector_current -= t_param * p_vector[ind_max] * diff
@@ -166,39 +169,72 @@ class MDM(object):
 
 
 
+#old 2-dim default example, works fine
+# points =np.array([[ -73.337555  ,   -4.82192605],
+#        [   9.36299101,   14.79378288],
+#        [  33.74875017,   10.02043701],
+#        [ 133.04981839,   92.18760616],
+#        [-105.00396348,  -69.46640213],
+#        [  32.54560694,   43.96449265],
+#        [ -78.01174375,   61.08025333],
+#        [  92.03366094,  -51.6208306 ],
+#        [  17.22114877,   54.92524147],
+#        [ -87.14266467,  128.5875058 ],
+#        [ -35.76597696, -161.63324815],
+#        [ 156.36709765,  -55.60266369],
+#        [  41.00897625,  -54.92133061],
+#        [ 129.50005618,  -39.14660553],
+#        [ 101.99767049,    5.91893179],
+#        [ 120.62635591,   39.32842524],
+#        [  58.91037616,  -29.52086718],
+#        [-116.99548555,  -35.64041842],
+#        [ -49.26778003,   18.11377985],
+#        [  91.22017504,   26.95527778],
+#        [   5.98350205,  -29.65544224],
+#        [  73.8606758 ,  -67.33527561],
+#        [ -57.11269196,  -23.38066312],
+#        [  10.29413585,   19.91249178],
+#        [ -76.57980277,   36.15112039],
+#        [  40.91217006,  -17.81387299],
+#        [  51.88700332,  -69.65988091],
+#        [  57.41048001, -119.28130887],
+#        [ -66.49323658,  -92.43371661],
+#        [  10.46455101,  -80.23934518]])   ##
 
-points =np.array([[ -73.337555  ,   -4.82192605],
-       [   9.36299101,   14.79378288],
-       [  33.74875017,   10.02043701],
-       [ 133.04981839,   92.18760616],
-       [-105.00396348,  -69.46640213],
-       [  32.54560694,   43.96449265],
-       [ -78.01174375,   61.08025333],
-       [  92.03366094,  -51.6208306 ],
-       [  17.22114877,   54.92524147],
-       [ -87.14266467,  128.5875058 ],
-       [ -35.76597696, -161.63324815],
-       [ 156.36709765,  -55.60266369],
-       [  41.00897625,  -54.92133061],
-       [ 129.50005618,  -39.14660553],
-       [ 101.99767049,    5.91893179],
-       [ 120.62635591,   39.32842524],
-       [  58.91037616,  -29.52086718],
-       [-116.99548555,  -35.64041842],
-       [ -49.26778003,   18.11377985],
-       [  91.22017504,   26.95527778],
-       [   5.98350205,  -29.65544224],
-       [  73.8606758 ,  -67.33527561],
-       [ -57.11269196,  -23.38066312],
-       [  10.29413585,   19.91249178],
-       [ -76.57980277,   36.15112039],
-       [  40.91217006,  -17.81387299],
-       [  51.88700332,  -69.65988091],
-       [  57.41048001, -119.28130887],
-       [ -66.49323658,  -92.43371661],
-       [  10.46455101,  -80.23934518]])
 
-dim = 2; number_of_points = 30
+points = np.array([[ -75.17801366,    7.83288242,   -5.01730323],
+       [  34.64493481,   17.08160686,   -4.12464737],
+       [  69.25185714,   46.30248975,  -50.02067627],
+       [  38.10853617,  -14.51657311,   16.07997019],
+       [ -11.14528316,  -23.9139584 ,  -48.72752102],
+       [  47.71039686,  -67.59707025,  -71.9969375 ],
+       [  28.54312988, -127.60998881, -114.56196853],
+       [ 205.10854027,    0.32344749,  -45.66628019],
+       [   2.92497051,  -46.89594309,   53.06491769],
+       [  41.97712067, -170.65770544,   18.60333687],
+       [ -51.55472502,   60.64006628,   67.86351622],
+       [  44.59497019,   27.57009689,   95.74464091],
+       [   3.35649724,  -12.96848999,  -67.41464425],
+       [ -47.15574201,   33.67097469,  -48.27886268],
+       [   7.66095838,   59.77078387, -162.02028844],
+       [ -69.59850196,  -23.95314366,  -58.41426815],
+       [  27.61525677,  -47.26875507,   -4.70457163],
+       [ -74.79462539,    7.96213077,   -9.01206742],
+       [ -66.99167694,   51.87158456,  -31.53336444],
+       [-116.95434661,  -95.79658366,  -22.96431562],
+       [  54.39636852,   25.06735714,   30.18068819],
+       [ -91.04888825,   58.20995254,  -24.17908215],
+       [ -12.8309548 ,  -16.78814715,  -41.21037765],
+       [ -78.25185047,   63.99086455,  -47.92050365],
+       [ -79.99177441,  -60.06975075,   29.71067655],
+       [  74.3836634 ,  -28.15199117,  -29.7868583 ],
+       [ -65.77372521,   79.95508642,    0.29089113],
+       [   5.23160557,  151.42554231,   60.96004149],
+       [ -49.1255094 ,   23.05475224,  181.98787966],
+       [ -46.1174773 ,  -31.61406696,   60.10877562]])
+
+
+dim = 3; number_of_points = 30
 
 isManualEnter = False
 isAccelerated = True
